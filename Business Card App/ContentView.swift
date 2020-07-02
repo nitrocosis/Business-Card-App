2d4489d1ebd9974373d6517f13702ba27ec635d8
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-07-02 13:09:12 -0500

    Initial commit

---
+++ Business Card App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Business Card App
+//
+//  Created by Sarah Rebecca Estrellado on 7/2/20.
+//  Copyright © 2020 Sarah Rebecca Estrellado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
